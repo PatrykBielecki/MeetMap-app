6512ae99cd046c5f17ee8ad9757720857a6a02a7
Patryk Bielecki <bielecki.biz@gmail.com>
2024-09-09 20:49:58 +0200

    Connect to gcloud api

--- requests/axiosConfig.tsx
+++ requests/axiosConfig.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.0.186:8080/api',
+  //baseURL: 'http://192.168.0.186:8080/api',
+  baseURL: 'http://34.118.3.10:8080/api',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 });
